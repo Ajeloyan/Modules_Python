9168a010c154351a659ff63edf63a6b6bf8e263f
Armenag Jeloyan <a.jeloyan1@icloud.com>
2026-03-17 16:22:10 +0100

    add: artifactcard class

---
+++ Module07/ex1/ArtifactCard.py
@@ -0,0 +1,26 @@
+from ex0.Card import Card, Rarity
+
+
+class ArtifactCard(Card):
+    def __init__(
+        self, name: str,
+        cost: int,
+        rarity: Rarity,
+        durability: int,
+        effect: str
+    ) -> None:
+        super().__init__(name, cost, rarity)
+        self.durability = durability
+        self.effect = effect
+
+    def play(self, game_state: dict) -> dict:
+        game_state["available_mana"] -= self.cost
+        return {"card_played": self.name,
+                "mana_used": self.cost,
+                "effect": f"Permanent: {self.effect}"}
+
+    def activate_ability(self) -> dict:
+        return {
+            "ability_activated": self.name,
+            "effect_description": self.effect
+        }
