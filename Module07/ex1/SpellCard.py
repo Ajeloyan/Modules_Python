01a3f4a9094506cfac797c448efd5d3f3653e984
Armenag Jeloyan <a.jeloyan1@icloud.com>
2026-03-20 23:11:22 +0100

    fix: spells now has power

--- Module07/ex1/SpellCard.py
+++ Module07/ex1/SpellCard.py
@@ -11,40 +11,42 @@ class SpellEffect(Enum):
 
 class SpellCard(Card):
     def __init__(
-        self, name: str, cost: int, rarity: Rarity, effect_type: str
+        self,
+        name: str,
+        cost: int,
+        rarity: Rarity,
+        effect_type: str,
+        power: int
     ) -> None:
         super().__init__(name, cost, rarity)
         self.effect_type = effect_type
+        self.power = power
 
     def play(self, game_state: dict) -> dict:
         game_state["available_mana"] -= self.cost
         result = {
             "card_played": self.name,
             "mana_used": self.cost,
-            "effect": "No specific effect"
+            "effect": "No specific effect",
         }
         if self.effect_type == SpellEffect.DAMAGE.value:
-            result.update({
-                "effect": f"Deal {self.cost} damage to target"
-            })
+            result.update({"effect": f"Deal {self.power} damage to target"})
         elif self.effect_type == SpellEffect.BUFF.value:
-            result.update({
-                "effect": f"Increase target attack +{self.cost}"
-            })
+            result.update({"effect": f"Increase target attack +{self.power}"})
         elif self.effect_type == SpellEffect.DEBUFF.value:
-            result.update({
-                "effect": f"Decrease target attack -{self.cost}"
-            })
+            result.update({"effect": f"Decrease target attack -{self.power}"})
         elif self.effect_type == SpellEffect.HEAL.value:
-            result.update({
-                "effect": f"Heal target by {self.cost}hp"
-            })
+            result.update({"effect": f"Heal target by {self.power}hp"})
         return result
 
     def resolve_effect(self, targets: list) -> dict:
+        for target in targets:
+            if self.effect_type == SpellEffect.DAMAGE.value:
+                target.health -= self.power
         return {
             "spell": self.name,
             "type": self.effect_type,
+            "power": self.power,
             "targets_affected": [target.name for target in targets],
             "status": "Effect resolved",
         }
