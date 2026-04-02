361cec2b7e04a31625256d6f0b57c9bcc9d17866
Ajeloyan <166011252+Ajeloyan@users.noreply.github.com>
2026-04-02 17:19:06 +0200

    add: starting ex1 with callable args kwargs

---
+++ Module10/ex1/higher_magic.py
@@ -0,0 +1,22 @@
+def fireball(target: str) -> str:
+    return (f"fireball hits {target}")
+
+
+def heal(target: str) -> str:
+    return (f"{target} healed")
+
+
+def spell_combiner(spell1: callable, spell2: callable) -> callable:
+    pass
+
+
+def power_amplifier(base_spell: callable, multiplier: int) -> callable:
+    pass
+
+
+def conditional_caster(condition: callable, spell: callable) -> callable:
+    pass
+
+
+def spell_sequence(spells: list[callable]) -> callable:
+    pass
